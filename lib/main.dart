a91d37d698ca513055a7a77f0387399a3f2b9176
Earnest Long, Jr <ealong@alumni.upenn.edu>
2020-05-04 20:55:32 -0500

    Two routes

--- lib/main.dart
+++ lib/main.dart
@@ -21,43 +21,20 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.amber,
         backgroundColor: Colors.black54,
       ),
-      home: MyHomePage(title: 'Login Page'),
+      initialRoute: '/',
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        '/': (context) => LoginScreen(),
+        // When navigating to the "/second" route, build the SecondScreen widget.
+        '/second': (context) => MapScreen(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class LoginScreen extends StatelessWidget {
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
-
-  void _toggleGenres() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      genresVisible = !genresVisible;
-    });
-  }
+  final TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
 
   @override
   Widget build(BuildContext context) {
@@ -86,7 +63,9 @@ class _MyHomePageState extends State<MyHomePage> {
       child: MaterialButton(
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {},
+        onPressed: () {
+          Navigator.pushNamed(context, '/second');
+        },
         child: Text("Login",
             textAlign: TextAlign.center,
             style: style.copyWith(
@@ -131,11 +110,29 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-Widget _myWidget(BuildContext context) {
-  String myString = 'I ❤ Flutter';
-  print(myString);
-  return Text(
-    myString,
-    style: TextStyle(fontSize: 30.0),
-  );
+class MapScreen extends StatelessWidget {
+
+  GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Map Screen"),
+      ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 11.0,
+        ),
+      ),
+    );
+  }
 }
